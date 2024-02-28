623b488acff932281d058958acc545f8f4989617
jainoddin <skjainoddin39854@gmail.com>
2024-02-28 13:49:58 +0530

    Deployment

--- frontend/src/hooks/useListenMessages.js
+++ frontend/src/hooks/useListenMessages.js
@@ -2,7 +2,7 @@ import { useEffect } from 'react';
 import { useSocketContext } from '../context/SocketContext';
 import useConversation from '../zustand/useConversation';
 
-import notificationSound from '../assets/sounds/notifications.mp3'
+import notificationSound from '../assets/sounds/notifications.mp3';
 
 const useListenMessages = () => {
   const { socket } = useSocketContext();
